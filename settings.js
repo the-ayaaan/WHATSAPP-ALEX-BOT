416b87838d576dcef667b638ddf26175d5418342
the-ayaaan <thehunteer9@gmail.com>
2026-07-19 13:32:13 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923037993871',
+    botName: '𝗠𝗛 𝗧𝗘𝗖𝗛 𝗠𝗗 𝗕𝗢𝗧',
+    ownerName: 'MR. HANAN'
+};
